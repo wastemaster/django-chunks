0c6c37874e2f02c7cf4cdb368b6bc56fdafca98b
Ruslan Popov <ruslan.popov@gmail.com>
2014-09-04 23:01:42 +0400

    For django-modeltranslation 0.7.

---
+++ chunks/translation.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from modeltranslation.translator import translator, TranslationOptions
+from . import models
+
+
+class ChunkOpts(TranslationOptions):
+    fields = ('content', )
+translator.register(models.Chunk, ChunkOpts)
+
+
+class ChunkGroupOpts(TranslationOptions):
+    fields = ('content', )
+translator.register(models.Group, ChunkGroupOpts)
+
+
+class ChunkMediaOpts(TranslationOptions):
+    fields = ('title', 'desc', 'media', )
+translator.register(models.Media, ChunkMediaOpts)
